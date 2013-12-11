7f35423848f892a82d3515e195dd1b4e76fb558b
1000ch <orz1000ch@gmail.com>
2013-12-11 21:56:07 +0900

    update

--- bin/cssdoc.js
+++ bin/cssdoc.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 
-var commander = require('commander');
+// external lib
+var argv = require('optimist').argv;
+var marked = require('marked');
 
-commander
-  .version('0.0.1')
-  .parse(process.argv);
+// own lib
+var file = require('../lib/file');
 
-console.log(commander.args);
+// target css files
+var files = [];
+
+// if argument is not specified
+if (argv._.length === 0) {
+  throw new Error('There is no argument.');
+}
+
+// if there is no css files
+argv._.filter(function(path) {
+  return file.exists(path);
+}).forEach(function(path) {
+  files.push(path);
+});
+
+if (files.length === 0) {
+  throw new Error('No css file is specified.');
+}
